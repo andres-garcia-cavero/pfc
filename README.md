c64a7b225030b799dde2a1826db97b609c3ff58b
belsierre <github@belsierre.org>
2017-06-04 22:54:14 +0200

    Init
    First GUI and first libnet script

---
+++ README.md
@@ -0,0 +1,13 @@
+# Proyecto de Fin de Carrera
+
+## Multimeter
+Herramienta para el testeo desatendido de una red
+
+Es una interfaz gráfica para mostrar los resultados de herramientas que ejecuta en los destinos a los que se ejecuta por SSH
+
+Algunas de las herramientas son:
+- D-ITG
+- Iperf
+
+## Flipsec
+Implementación para redireccionar tráfico IPsec "al vuelo" utilizando "libnetqueue" filtering 
---
+++ multimeter/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { ConfigComponent } from './config/config.component';
+import { CreditsComponent } from './credits/credits.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'config', component: ConfigComponent },
+  { path: 'credits', component: CreditsComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ConfigComponent,
+    CreditsComponent,
+    HomeComponent
+  ],
+  imports: [
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
